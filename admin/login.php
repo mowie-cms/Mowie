c8a19d3b3c4ae4bdfbff04ca8793cabc03c54a6b
kolaente <konrad@kola-entertainments.de>
2017-10-04 16:38:44 +0200

    Fixed login bug

--- admin/login.php
+++ admin/login.php
@@ -12,28 +12,26 @@ if(isset($_POST['username']))
 	$db->get();
 	if (isset($db->data[0]) && $db->data[0]['id'] != '')
 	{
-		if (password_verify($_POST['pw'], $db->data[0]['pass']))
+		$userData = $db->data[0];
+		if (password_verify($_POST['pw'], $userData['pass']))
 		{
 			session_regenerate_id();
-			$uid = $db->data[0]['id'];
 
 			//Token
 			$token = random(64);
 			$_SESSION['token'] = $token;
 			$db->clear();
 			$db->setCol('system_loggedin');
-			$db->data['user'] = $uid;
+			$db->data['user'] = $userData['id'];
 			$db->data['user_agent'] = $_SERVER['HTTP_USER_AGENT'];
 			$db->data['ip'] = $_SERVER['REMOTE_ADDR'];
 			$db->data['time'] = time();
 			$db->data['token'] = $token;
-			//if($db->insert()) echo 'token'; else echo 'hm';
 			$db->insert();
-			//echo $token;
 
 			$db->clear();
 			$db->setCol('system_admins');
-			$db->data['id'] = $uid;
+			$db->data['id'] = $userData['id'];
 			$db->get();
 			if ($db->data[0]['secret'] != '')
 			{
@@ -62,10 +60,10 @@ if(isset($_POST['username']))
 			}
 
 			//Session
-			$_SESSION['user'] = $db->data[0]['username'];
-			$_SESSION['userid'] = $db->data[0]['id'];
-			$_SESSION['lvl'] = $db->data[0]['lvl'];
-			$_SESSION['mail'] = $db->data[0]['mail'];
+			$_SESSION['user'] = $userData['username'];
+			$_SESSION['userid'] = $userData['id'];
+			$_SESSION['lvl'] = $userData['lvl'];
+			$_SESSION['mail'] = $userData['mail'];
 			$_SESSION['guestview'] = 'true';
 
 			stream_message('{user} has logged in.', 4);
