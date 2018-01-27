7c1f581ff1187287a8df570df3c0b7302540b717
kolaente <konrad@kola-entertainments.de>
2018-01-27 16:48:13 +0100

    Fixed not including composer autoload when resetting admin password

--- admin/reset-pw.php
+++ admin/reset-pw.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../vendor/autoload.php'
+require_once '../vendor/autoload.php';
 require_once '../inc/config.php';
 require_once '../inc/libs/functions.php';
 $lang->setLangFolder('lang/');
