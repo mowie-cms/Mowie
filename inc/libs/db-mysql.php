ec44bdce88a07d5529b9c16c12cd16e78c350813
kolaente <konrad@kola-entertainments.de>
2018-02-18 19:07:11 +0100

    Fixed nav-app installation not working

--- inc/libs/db-mysql.php
+++ inc/libs/db-mysql.php
@@ -154,6 +154,8 @@ class db
 				}
 				$stmt .= ') VALUES (' . $valCnt . ')';
 
+				echo $stmt;
+				print_r($vals);
 				$insert = $this->dbh->prepare($stmt);
 				return $insert->execute($vals);
 			}
