71743d5f46568717c02476d9b321a53fce650a74
kolaente <konrad@kola-entertainments.de>
2018-02-18 19:00:09 +0100

    Fixed nav-app installation not working

--- apps/Navigation/config.php
+++ apps/Navigation/config.php
@@ -4,6 +4,7 @@ $_CONF['app_build'] = 2;
 $_CONF['app_version'] = 'v0.91 Beta';
 $_CONF['base_file'] = 'nav.php';
 $_CONF['type'] = 'static';
+$_CONF['install'] = 'install.php';
 
 $_CONF['menu_top'] = '<i class="fa fa-bars"></i>  ' . $lang->get('nav_title');
 $_CONF['menu'] = ['menu_top' => 'index.php'];
